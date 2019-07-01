4902e4ec18b30b6f5460eb6133e2eea5adb077ca
Christopher Hoover <ch@murgatroid.com>
2019-06-30 17:38:43 -0700

    aprx checkpoint

--- aprs/aprx/run_rx_igate.sh
+++ aprs/aprx/run_rx_igate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+set -o xtrace
+set -o errexit
+
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-aprx -v -d -L -f "${HERE}/aprx_rx_igate.conf"
+TMPCONF=$(mktemp)
+trap "rm -f $TMPCONF" EXIT
+
+if [ "x$1" == "x" ]; then
+    echo "usage: $0 port" 1>&2
+    exit 1
+fi
+
+perl -p -e "s@XXPORTXX@$1@g" < "${HERE}/aprx_rx_igate.conf" >"${TMPCONF}"
+
+aprx -i -v -d -d -d -L -f "${TMPCONF}"
