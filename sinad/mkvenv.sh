cb71628f8333e4d7406ee13f4837a6afe9e68db8
Christopher Hoover <ch@murgatroid.com>
2025-03-10 19:35:30 -0700

    Cleans up venv set up.

--- sinad/mkvenv.sh
+++ sinad/mkvenv.sh
@@ -3,8 +3,10 @@
 set -o errexit
 set -o nounset
 
-python3 -m venv .
-. bin/activate
+rm -fr .venv pydwf-examples
+
+python3 -m venv .venv
+. .venv/bin/activate
 pip install pydwf
 pip install matplotlib
 pip install scipy
